dc1c8e71b137a7e30ba1881d30e5203129a08498
ohgong0103 <truyenduong0103@gmail.com>
2025-11-03 02:39:43 +0700

    feat(auth): implement JWT-based authentication
    - Added AuthController with login and register endpoints
    - Configured JWT token filter and exception handler
    - Updated UserService to support authentication

---
+++ src/main/java/com/example/identity_service/service/AuthenticationService.java
@@ -0,0 +1,86 @@
+package com.example.identity_service.service;
+
+import com.example.identity_service.dto.request.AuthenticationRequest;
+import com.example.identity_service.dto.request.IntrospectRequest;
+import com.example.identity_service.dto.response.AuthenticationResponse;
+import com.example.identity_service.dto.response.IntrospectResponse;
+import com.example.identity_service.exception.AppException;
+import com.example.identity_service.exception.ErrorCode;
+import com.example.identity_service.repository.UserRepository;
+import com.nimbusds.jose.*;
+import com.nimbusds.jose.crypto.MACSigner;
+import com.nimbusds.jose.crypto.MACVerifier;
+import com.nimbusds.jwt.JWTClaimsSet;
+import com.nimbusds.jwt.SignedJWT;
+import lombok.AccessLevel;
+import lombok.RequiredArgsConstructor;
+import lombok.experimental.FieldDefaults;
+import lombok.experimental.NonFinal;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.stereotype.Service;
+
+import java.text.ParseException;
+import java.time.Instant;
+import java.time.temporal.ChronoUnit;
+import java.util.Date;
+
+@Slf4j
+@Service
+@RequiredArgsConstructor
+@FieldDefaults(level= AccessLevel.PRIVATE, makeFinal = true)
+public class AuthenticationService {
+    UserRepository userRepository;
+    @NonFinal
+    protected static final String SIGNER_KEY="62560d321634fd7f9dc4fd14b0ccb14045c9c8df19127163adec06555fb9b2b6";
+    public IntrospectResponse introspect(IntrospectRequest request) throws JOSEException, ParseException {
+        var token = request.getToken();
+
+        JWSVerifier verifier = new MACVerifier(SIGNER_KEY.getBytes());
+        SignedJWT signedJWT = SignedJWT.parse(token);
+        Date expityTime=signedJWT.getJWTClaimsSet().getExpirationTime();
+        var verified= signedJWT.verify(verifier);
+        return IntrospectResponse.builder()
+                .valid(verified && expityTime.after(new Date()))
+                .build();
+
+    }
+    public AuthenticationResponse authenticate(AuthenticationRequest request){
+        var user=userRepository.findByUsername(request.getUsername())
+                .orElseThrow(()-> new AppException(ErrorCode.USER_UNEXISTS));
+        PasswordEncoder passwordEncoder = new BCryptPasswordEncoder(10);
+        boolean authenticated =  passwordEncoder.matches(request.getPassword(), user.getPassword());
+        if(!authenticated)
+            throw new AppException(ErrorCode.UNAUTHENTICATED);
+
+        var token = generateToken(request.getUsername());
+        return AuthenticationResponse.builder()
+                .token(token)
+                .authenticated(true)
+                .build();
+
+    }
+    private String generateToken(String username){
+        JWSHeader jwsHeader = new JWSHeader(JWSAlgorithm.HS256);
+        JWTClaimsSet jwtClaimsSet = new JWTClaimsSet.Builder()
+                .subject(username)
+                .issuer("google.com")
+                .issueTime(new Date())
+                .expirationTime(new Date(
+                        Instant.now().plus(1, ChronoUnit.HOURS).toEpochMilli()
+                ))
+                .build();
+        Payload payload = new Payload(jwtClaimsSet.toJSONObject());
+        JWSObject jwsObject = new JWSObject(jwsHeader,payload);
+        try {
+            jwsObject.sign(new MACSigner(SIGNER_KEY.getBytes()));
+            return jwsObject.serialize();
+        } catch (JOSEException e) {
+            log.error("can not create token");
+            throw new RuntimeException(e);
+        }
+
+    }
+
+}
